29c0334983ed18e8b0e6bcedee7e81e4c40b7847
m0su <sirm0su@gmail.com>
2023-05-01 16:54:19 +0900

    Initial Commit

---
+++ jarvis/jarvisApp.swift
@@ -0,0 +1,17 @@
+//
+//  jarvisApp.swift
+//  jarvis
+//
+//  Created by seo on 2023/05/01.
+//
+
+import SwiftUI
+
+@main
+struct jarvisApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
